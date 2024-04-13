204a4609794b248e35303f34aaa964952be6d914
developer-aayu <aayusharma00123@gmail.com>
2024-04-13 21:08:34 +0530

    error in home and profile files

--- src/appRouter.jsx
+++ src/appRouter.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import Home from "./Components/NavComponents/Home.jsx";
-import SignUp from "./Components/LoginComponents/SignUp.jsx";
 import Error from './Components/Error/index.jsx'
 import Dashboard from "./Components/Home/Dashboard.jsx";
+import Profile from "./Components/profile/Profile.jsx";
 import { useAuth } from "./contexts/authContext/index.jsx";
 import {
   BrowserRouter as Router,
@@ -20,10 +20,13 @@ export default function appRouter() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/error" element={<Error />} />
+            
             {userLoggedIn ? (
-              <Route path="/home" element={<Dashboard />} />
+              <Route path="/home" element={<Dashboard />} />,
+              <Route path="/profile" element={<Profile />} />
               ) : (
-                <Route path="/home" element={<Navigate to="/error" />} />
+                <Route path="/home" element={<Navigate to="/error" />} />,
+                <Route path="/profile" element={<Navigate to="/error" />} />
                 )}
           </Routes>
         </Router>
